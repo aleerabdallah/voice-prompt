efa7267dff5380ddb82e4d72acc3d8f2360abb61
abdo-aleer <juniorab444@gmail.com>
2025-03-21 15:31:09 +0200

    Completed+: the messaging exchange layout

--- src/main.py
+++ src/main.py
@@ -6,15 +6,17 @@ async def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
     page.theme_mode = ft.ThemeMode.LIGHT
     page.title = "ChatBot"
-
+    page.bgcolor = "#212121"
     chat = ft.ListView(auto_scroll=True, expand=True, padding=10, spacing=10)
+
+    chat.controls.append(ft.Container(padding=20))
 
     chat.controls.append(
         ft.Row(
             [
                 ft.Container(
                     content=ft.Text(
-                        "Hello",
+                        "Hi, what's going on with you",
                         # text_align=ft.TextAlign.END,
                         color="white",
                         bgcolor="#2e3440",
@@ -22,12 +24,15 @@ async def main(page: ft.Page):
                     ),
                     bgcolor="#2e3440",
                     # alignment=ft.alignment.top_right,
-                    padding=0,
+                    padding=10,
+                    border_radius=20,
                 )
             ],
             alignment=ft.MainAxisAlignment.END,
         )
     )
+
+    new_message = ft.TextField(hint_text="Let's chat")
     chat.controls.append(ft.Text("Hey! How's it going", color="white"))
     page.add(chat)
 
