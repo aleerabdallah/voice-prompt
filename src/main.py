08936c07c84466dc4d487f648a36a8741caf8384
abdo-aleer <juniorab444@gmail.com>
2025-03-21 13:19:42 +0200

    Added+: static user's prompt and model response

--- src/main.py
+++ src/main.py
@@ -7,7 +7,29 @@ async def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.LIGHT
     page.title = "ChatBot"
 
-    chat = ft.ListView(auto_scroll=True, expand=True)
+    chat = ft.ListView(auto_scroll=True, expand=True, padding=10, spacing=10)
+
+    chat.controls.append(
+        ft.Row(
+            [
+                ft.Container(
+                    content=ft.Text(
+                        "Hello",
+                        # text_align=ft.TextAlign.END,
+                        color="white",
+                        bgcolor="#2e3440",
+                        expand=20,
+                    ),
+                    bgcolor="#2e3440",
+                    # alignment=ft.alignment.top_right,
+                    padding=0,
+                )
+            ],
+            alignment=ft.MainAxisAlignment.END,
+        )
+    )
+    chat.controls.append(ft.Text("Hey! How's it going", color="white"))
+    page.add(chat)
 
 
 ft.app(main)
