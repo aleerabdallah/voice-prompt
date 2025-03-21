b1ce44590b95e9f3ef7558fbf451f6d346025466
abdo-aleer <juniorab444@gmail.com>
2025-03-21 18:32:33 +0200

    +Message get's taken from input and added into the ListView

--- src/main.py
+++ src/main.py
@@ -13,37 +13,47 @@ async def main(page: ft.Page):
 
     chat.controls.append(ft.Container(padding=20))
 
-    chat.controls.append(
-        ft.Row(
-            [
-                ft.Container(
-                    content=ft.Text(
-                        "Hi, what's going on with you",
-                        # text_align=ft.TextAlign.END,
-                        color="white",
+    def send_prompt(e, message):
+        print(f"Hello: {e}")
+        print(message.value)
+        chat.controls.append(
+            ft.Row(
+                [
+                    ft.Container(
+                        content=ft.Text(
+                            f"{message.value}",
+                            # text_align=ft.TextAlign.END,
+                            color="white",
+                            bgcolor="#2e3440",
+                            expand=20,
+                        ),
                         bgcolor="#2e3440",
-                        expand=20,
-                    ),
-                    bgcolor="#2e3440",
-                    # alignment=ft.alignment.top_right,
-                    padding=10,
-                    border_radius=20,
-                )
-            ],
-            alignment=ft.MainAxisAlignment.END,
+                        # alignment=ft.alignment.top_right,
+                        padding=10,
+                        border_radius=20,
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.END,
+            )
         )
-    )
+        message.value = ""
+        page.update()
 
     new_message = ft.TextField(
         hint_text="Let's chat",
-        color="#2e3440",
+        color="#444444",
         max_lines=4,
         min_lines=1,
         expand=True,
         multiline=True,
         border=ft.InputBorder.NONE,
+        # on_submit=
     )
-    send_prompt_btn = ft.IconButton(icon=ft.Icons.ARROW_UPWARD, bgcolor="#2e3440")
+    send_prompt_btn = ft.IconButton(
+        icon=ft.Icons.ARROW_UPWARD,
+        bgcolor="#2e3440",
+        on_click=lambda e: send_prompt(e, new_message),
+    )
     add_btn = ft.IconButton(icon=ft.Icons.ADD, bgcolor="#2e3440")
     chat.controls.append(ft.Text("Hey! How's it going", color="white"))
     page.add(
@@ -60,7 +70,7 @@ async def main(page: ft.Page):
             ),
             bgcolor="#303030",
             border_radius=20,
-            padding=9,
+            padding=18,
             adaptive=True,
         ),
     )
