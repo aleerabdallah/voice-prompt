aa6a3065575889fd6a6add4604e475dc22f861a9
abdo-aleer <juniorab444@gmail.com>
2025-03-21 16:17:59 +0200

    Added+: Text input for prompting

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ async def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
     page.theme_mode = ft.ThemeMode.LIGHT
     page.title = "ChatBot"
+    page.padding = 0
+
     page.bgcolor = "#212121"
     chat = ft.ListView(auto_scroll=True, expand=True, padding=10, spacing=10)
 
@@ -32,9 +34,36 @@ async def main(page: ft.Page):
         )
     )
 
-    new_message = ft.TextField(hint_text="Let's chat")
+    new_message = ft.TextField(
+        hint_text="Let's chat",
+        color="#2e3440",
+        max_lines=4,
+        min_lines=1,
+        expand=True,
+        multiline=True,
+        border=ft.InputBorder.NONE,
+    )
+    send_prompt_btn = ft.IconButton(icon=ft.Icons.ARROW_UPWARD, bgcolor="#2e3440")
+    add_btn = ft.IconButton(icon=ft.Icons.ADD, bgcolor="#2e3440")
     chat.controls.append(ft.Text("Hey! How's it going", color="white"))
-    page.add(chat)
+    page.add(
+        chat,
+        ft.Container(
+            ft.Column(
+                [
+                    new_message,
+                    ft.Row(
+                        [add_btn, send_prompt_btn],
+                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                    ),
+                ]
+            ),
+            bgcolor="#303030",
+            border_radius=20,
+            padding=9,
+            adaptive=True,
+        ),
+    )
 
 
 ft.app(main)
